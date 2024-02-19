40e85a0695e710d285662a8c730c4b55b50a2610
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-02-19 12:58:53 -0500

    adding protocol buffer and graphql info

---
+++ Spring2024/lab4/APIs/graphQl.js
@@ -0,0 +1,45 @@
+const { ApolloServer, gql } = require("apollo-server"); // Tag function to parse the schema
+
+// GraphQL Schema.
+// It describes what our API can do
+const typeDefs = gql`
+  schema {
+    query: Query
+  }
+
+  type Query {
+    greeting: String
+  }
+`;
+
+/* Now typeDefs is an abstract syntax
+   tree of the Graphql code:
+    {
+      kind: 'Document',
+      definitions: [
+        {
+          kind: 'ObjectTypeDefinition',
+          description: undefined,
+          name: [Object],
+          interfaces: [],
+          directives: [],
+          fields: [Array]
+        }
+      ],
+      loc: { start: 0, end: 41 }
+    }
+*/
+
+// Resolver function to return the data.
+// It has to match our type definitions
+const resolvers = {
+  Query: {
+    greeting: () => "Hello GraphQL world!👋",
+  },
+};
+
+// Set up the server with ApolloServer
+const server = new ApolloServer({ typeDefs, resolvers });
+server
+  .listen({ port: 9000 })
+  .then(({ url }) => console.log(`Server running at ${url}`));
---
+++ Spring2024/lab4/APIs/protocolbufs.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+const express = require("express");
+const protobuf = require("protobufjs");
+
+const app = express();
+
+run().catch((err) => console.log(err));
+
+async function run() {
+  const root = await protobuf.load("user.proto");
+
+  const doc = { name: "Bill", age: 30 };
+  const User = root.lookupType("userpackage.User");
+
+  app.get("/user", function (req, res) {
+    res.send(User.encode(doc).finish());
+  });
+
+  app.post("/user", express.text({ type: "*/*" }), function (req, res) {
+    // Assume `req.body` contains the protobuf as a utf8-encoded string
+    const user = User.decode(Buffer.from(req.body));
+    Object.assign(doc, user);
+    res.end();
+  });
+
+  await app.listen(3000);
+
+  let data = await axios
+    .get("http://localhost:3000/user")
+    .then((res) => res.data);
+
+  // "Before POST User { name: 'Bill', age: 30 }"
+  console.log("Before POST", User.decode(Buffer.from(data)));
+  const postBody = User.encode({ name: "Joe", age: 27 }).finish();
+  await axios
+    .post("http://localhost:3000/user", postBody)
+    .then((res) => res.data);
+
+  data = await axios.get("http://localhost:3000/user").then((res) => res.data);
+  // "After POST User { name: 'Joe', age: 27 }"
+  console.log("After POST", User.decode(Buffer.from(data)));
+}
